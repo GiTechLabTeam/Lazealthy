88306ab888f89862fb1aacb235c8d08f802b2259
Yang SHEN <yang.shen@commscope.com>
2016-06-12 17:05:15 +0800

    Initialized Lazealthy Desktop Code

---
+++ Desktop/Code/Lazealthy/Lazealthy.Service/MainService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lazealthy.Service
+{
+    public partial class MainService : ServiceBase
+    {
+        public MainService()
+        {
+            InitializeComponent();
+        }
+
+        #region Proxy
+        public void StartService(string[] args) { OnStart(args); }
+
+        public void StopService() { OnStop(); }
+        #endregion
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
---
+++ Desktop/Code/Lazealthy/Lazealthy.Service/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lazealthy.Service
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            if (Environment.UserInteractive)
+            {
+                var service = new MainService();
+                service.StartService(args);
+                Console.WriteLine("Lazealthy service is running. Press any key to quit.");
+                Console.ReadKey();
+                service.StopService();
+            }
+            else
+            {
+                var ServicesToRun = new ServiceBase[] { new MainService() };
+                ServiceBase.Run(ServicesToRun);
+            }
+        }
+    }
+}
