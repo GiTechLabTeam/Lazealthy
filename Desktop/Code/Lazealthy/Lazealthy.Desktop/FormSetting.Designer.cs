1747e5b7ee2dceff3bad71f78355bc80f344fa20
Yang SHEN <yang.shen@commscope.com>
2016-06-13 11:01:28 +0800

    Add Icon for the Desktop App.

--- Desktop/Code/Lazealthy/Lazealthy.Desktop/FormSetting.Designer.cs
+++ Desktop/Code/Lazealthy/Lazealthy.Desktop/FormSetting.Designer.cs
@@ -28,9 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormSetting));
+            this.SuspendLayout();
+            // 
+            // FormSetting
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormSetting";
+            this.Text = "Lazealthy";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
